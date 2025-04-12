bc1b60cee823eb2c9e4a2184f688d6433bb2c373
InVorker <120738638+InWorker1@users.noreply.github.com>
2025-04-13 02:21:18 +0500

    Create main2.c

---
+++ main2.c
@@ -0,0 +1,122 @@
+#include <stdio.h>
+// #include <conio.h>
+#include "string.h"
+#include <time.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+
+int getch(void) {
+    struct termios oldt, newt;
+    int ch;
+    
+    // Получаем текущие настройки терминала
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    
+    // Отключаем канонический режим и эхо
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    
+    // Получаем символ
+    ch = getchar();
+    
+    // Восстанавливаем настройки терминала
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    
+    return ch;
+}
+
+void remch(char *s, int index) {
+    for (int i = index; s[i]; i++)
+        s[i] = s[i + 1];
+}
+
+
+
+char getcard(char a, int *sum, int *b) {
+    // printf("\n in fuck -- %c, %d, %d, %d\n", a, *b, *sum, (int)a);
+    int cif = a - '0';
+    switch (cif) {
+            case 1: printf("туз\n"); *sum+=11; break;
+            case 0: printf("10\n"); *sum+=10; break;
+            case 2: printf("валет\n"); *sum+=cif; break;
+            case 3: printf("дама\n"); *sum+=cif; break;
+            case 4: printf("кароль\n"); *sum+=cif; break;
+            default: printf("%c\n", a); *sum+=cif;
+        }
+    *b-=1;
+
+    return 0;
+}
+
+int random_int(int max) {
+    // int min = 0;
+    // return min + arc4random_uniform(max - min + 1);
+    srand(time(NULL));
+    int a = rand() % (max + 1);
+    return a;
+}
+
+int main(void)
+{
+    int m = 35;
+    int sum_user = 0; 
+    int sum_pc = 0;
+    
+    char cards[] = "00003333444466667777888899991111";
+    printf("%s\n", cards);
+    printf("Привет дружа! Сыграем в 21?)) да или нет\n -> ");
+    char otv[4]; 
+    scanf("%s", otv);
+    int otv1 = strcmp(otv, "да");
+    
+
+    if (otv1 == 0) {
+        printf("Я покажу свою карту и выдам тебе две карты. Ты сможешь выбрать между: оставить (< = >) или продолжить(<Enter>)\n");
+        
+
+        int c = random_int(m);
+        printf("rand cif %d\n", c);
+        char card = cards[c];
+        
+        printf("Моя первая карта ");
+        
+        getcard(card, &sum_pc, &m);
+        remch(cards, c);
+        
+        // printf("%s\n", cards);
+        
+        
+        char otv2[20];
+        getch();
+        while (getch() != '=') {
+            c = random_int(m);
+            card = cards[c];
+            printf("Твоя карта ");
+            getcard(card, &sum_user, &m);
+            printf("Сумма игрока: %d\n", sum_user);
+            remch(cards, c);
+            if (sum_user == 21) {
+                printf("YOU WIN");
+                return 0;
+            }
+            else if (sum_user > 21) {
+                printf("YOU LOSE");
+                return 0;
+            }
+            else {
+                continue;
+            }
+        }
+        printf("моя вторая карта ");
+        getcard(card, &sum_pc, &m);
+        printf("Сумма банкира: %d\n", sum_pc);
+        remch(cards, card);
+        // if (sum_user )
+    }
+    else {
+        printf("Ты видимо не из понятливых. Заново запускай и пиши да.");
+    }
+ return 0;
+}
